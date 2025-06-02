37bb649e2139fd5b37748999c10ef74fb44fbb5d
Even <evenifumiss@musicbravo.net>
2025-06-02 14:31:28 +0800

    Add .svelte-kit to gitignore and untrack its contents

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.Bozamwv4.js","_app/immutable/chunks/sGujlNqX.js","_app/immutable/chunks/DkfcLkkR.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.eRIXyVVQ.js","_app/immutable/chunks/sGujlNqX.js","_app/immutable/chunks/DkfcLkkR.js","_app/immutable/chunks/B_AqWkID.js","_app/immutable/chunks/BvHBzDWs.js","_app/immutable/chunks/DauwXo1R.js","_app/immutable/chunks/CtfEvnOT.js"];
-export const stylesheets = [];
-export const fonts = [];
